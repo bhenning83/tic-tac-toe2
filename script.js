1c13ebd4b79ecd16412a8be8fc61ec0c91f30e9f
Brendon Henning <brendonhenning@gmail.com>
2020-11-23 15:43:15 -0600

    able to change markers on board

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
-let test = document.querySelector('.test')
-const func = (e) => {
-  console.log(e)
-}
-test.addEventListener('click', func)
+const Gameboard = (function() {
+  let board = ["X", "O", "X", "O", "X", "O", "X", "O","X"];
 
+  //cache DOM
+  let boardContainer = document.querySelector("#board-container");
+
+  function createBoard() {
+    for (i = 0; i < board.length; i++) {
+      let square = document.createElement('li');
+      square.setAttribute("id", `square-${i}`);
+      square.setAttribute("class", "square");
+      boardContainer.appendChild(square)
+    }
+    render();
+  }
+  
+  function render() {
+    let squares = Gameboard.boardContainer.getElementsByTagName("li");
+    squares = Array.prototype.slice.call( squares )
+    console.log(squares)
+    for (i = 0; i < board.length; i++) {
+      squares[i].innerText = board[i];
+    }
+  }
+
+  return {
+    createBoard,
+    render,
+    boardContainer,
+    board
+  };
+})();
+
+const Player = (symbol) => {
+
+  //cache DOM
+  let squares = Gameboard.boardContainer.getElementsByTagName("li");
+  squares = Array.prototype.slice.call( squares )
+
+  //bind events
+  squares.forEach(square => {
+    square.addEventListener("click", playTurn)
+  })
+
+  function playTurn() {
+    id = Number(this.id.slice(-1)); //returns id number
+    Gameboard.board[id] = symbol;
+    console.log(symbol)
+    console.log(Gameboard.board)
+    Gameboard.render();
+  }
+
+  return {
+    playTurn
+  }
+};
+
+const GamePlay = (function() {
+
+})();
+Gameboard.createBoard()
+const b = Player("B");

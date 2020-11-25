33c91e0a9d4674cff3be55960c2a456ba87bc9a2
Brendon Henning <brendonhenning@gmail.com>
2020-11-24 18:06:41 -0600

    game works and stops at winner correctly

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const GameBoard = (function() {
-  let board = ["X", "O", "X", "O", "X", "O", "X", "O","X"];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-  //cache DOM
+  //Cache DOM
   squares = document.querySelectorAll(".square");
 
   function render() {
@@ -9,37 +9,89 @@ const GameBoard = (function() {
       squares[i].innerText = board[i];
     }
   }
-
-  return {
-    render,
-    board,
-    squares
-  };
+  
+  return {render, board, squares, reset};
 })();
 
 const Player = (marker) => {
+  winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  let playLog = [];
+  
+  function checkForWinner() {
+    winner = []
+    winning_combos.map(combo => { //Iterates through winning combos
+      results = [];
+      combo.map(x => {
+        if (playLog.includes(x)) results.push(x)
+      }); 
+      if (results.length == 3) winner.push(results); //If all three numbers in winning combo are in player's play
+    });
+    return winner
+  }
 
-  squares = Array.prototype.slice.call( GameBoard.squares ); //turns HTML collection into array
+  return {checkForWinner, playLog, marker}
+};
 
-  //bind events
-  squares.forEach(square => {
-    square.addEventListener("click", playTurn)
-  })
+
+const Game = (function() {
+  config = {};
+
+  //Cache DOM
+  winnerBox = document.querySelector(".winner-box");  
+  //Turns HTML collection into array
+  squares = Array.prototype.slice.call( GameBoard.squares );
+
+  function startGame() {
+    squares.forEach(square => {
+      square.addEventListener("click", playTurn)
+    });
+    setConfig();
+  }
 
   function playTurn() {
-    id = Number(this.id.slice(-1)); //returns id number
-    GameBoard.board[id] = marker;
+    makePlay.bind(this)();
+    combo = config.currentPlayer.checkForWinner();
+    if (combo.length == 1) {
+      announceWinner(combo);
+      endGame();
+    }
+    changePlayer();
+  }
+
+  function makePlay() {
+    id = Number(this.id.slice(-1)); //returns id number of square
+    if (GameBoard.board[id] === "") { //prevents play in already taken square
+      GameBoard.board[id] = config.currentPlayer.marker;
+      config.currentPlayer.playLog.push(id);
+    }
     GameBoard.render();
   }
 
-  return {
-    playTurn
+  function setConfig() {
+    player1 = Player("X")
+    player2 = Player("O")
+    config = {player1, player2, currentPlayer: player1}
   }
-};
-const GamePlay = (function() {
 
+  function changePlayer() {
+    if (config.currentPlayer == config.player1) {
+      config.currentPlayer = config.player2
+    } else {
+      config.currentPlayer = config.player1
+    }
+  }
+
+  function announceWinner(combo) {
+    winnerBox.innerText = `${config.currentPlayer} is the winner!`
+  }
+
+  function endGame() {
+    squares.forEach(square => {
+      square.removeEventListener("click", playTurn)
+    });
+  }
+
+  return {startGame}
 })();
 
-
-GameBoard.render();
-const b = Player("B");
+Game.startGame();
